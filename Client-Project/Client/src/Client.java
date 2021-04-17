bd73054040f92e6e164432d4785832b7aa46b08e
JDC0DE <joshua.devine24@gmail.com>
2021-04-17 23:31:55 +1000

    Updated Client to Make a Gets Largest Server Method
    The client code has been updated so that the code for the getting the server information has been moved to its own function that is called now before the main while loop. An additional function has been created which satisfies the algorithm allToLargest where the server list info arrayList is looped and split and held in a temporary array which updates through each loop of the list. New ServerInfo objects are created with each loop that have their attributes assigned to the corresponding positions of the array. These new objects are then added into their own arrayList where it is looped through and the corecount is checked to see which is the largest the global int are then updated to hold largest corecount, server ID and server type.

--- Client-Project/Client/src/Client.java
+++ Client-Project/Client/src/Client.java
@@ -14,6 +14,74 @@ public class Client {
     private static String SCHD = "SCHD";
     private static String QUIT = "QUIT";
     private static final String dot = ".";
+    private static int biggestCS = 0;
+    private static int biggestSID = 0;
+    private static int counter = 0;
+    private static String biggestST = "";
+
+
+    public static void createServerInfo(ArrayList<String> SLI) {
+        String[] SLIHold;
+        String res = null;
+        ArrayList<ServerInfo> serverHold = new ArrayList<>();
+        for (int i = 0; i < SLI.size(); i++) {
+            ServerInfo si = new ServerInfo();
+            SLIHold = SLI.get(i).split("\\s+");
+            System.out.println("SLI:" + SLIHold);
+            si.type = SLIHold[0];
+            si.id = Integer.parseInt(SLIHold[1]);
+            si.coreCount = Integer.parseInt(SLIHold[4]);
+            si.memory = Integer.parseInt(SLIHold[5]);
+            si.disk = Integer.parseInt(SLIHold[6]);
+            serverHold.add(si);
+            System.out.println("si:" + si);
+        }
+            biggestCS = serverHold.get(0).coreCount;
+            biggestSID = serverHold.get(0).id;
+            biggestST = serverHold.get(0).type;
+        for (int i = 0; i < serverHold.size(); i++) {
+            counter = i;
+            if (serverHold.get(i).coreCount > biggestCS) {
+                biggestCS = serverHold.get(i).coreCount;
+                biggestSID = serverHold.get(i).id;
+                biggestST = serverHold.get(i).type;
+            }
+        }
+        //return res;
+
+    }
+
+    public static void getLargestServer(Socket s, PrintWriter pw, BufferedReader bf) {
+        String reply = "";
+        ArrayList<String> SLI = new ArrayList<>();
+        //ArrayList<ServerInfo> serverHold = new ArrayList<>();
+        try {
+            pw.println(GETS);
+            pw.flush();
+            reply = bf.readLine();
+            System.out.println("server outer : " + reply);
+            while (!reply.equals(dot)) {
+                pw.println(OK);
+                pw.flush();
+                reply = bf.readLine();
+                System.out.println("server inner: " + reply);
+                if (!reply.equals(dot)) {
+                    SLI.add(reply);
+                    System.out.println(SLI);
+                    
+                }
+            }
+                
+            // for (int i = 0; i < serverHold.size(); i++) {
+            //     System.out.println("here:" + serverHold.get(i).type);
+            // }
+           // reply = 
+            createServerInfo(SLI);
+        } catch (Exception e) {
+            System.out.println("Error: ArrayList invalid");
+        }
+        //return reply;
+    }
 
     public static void main(String[] args) throws IOException, SocketException{
         
@@ -52,29 +120,7 @@ public class Client {
         System.out.println("server : " + str);
         
         while(str.equals(NONE)){
-            String reply = "";
-            String[] SLIHold;
-            if (userCmd.equals("Get Server Information")) {
-                pw.println(GETS);
-                pw.flush();
-                reply = bf.readLine();
-                System.out.println("server outer : " + reply);
-                while (!reply.equals(dot)) {
-                    pw.println(OK);
-                    pw.flush();
-                    reply = bf.readLine();
-                    System.out.println("server inner: " + reply);
-                    if (!reply.equals(dot)) {
-                        SLI.add(reply);
-                        System.out.println(SLI);
-                    }
-
-                }
-               
-
-                userCmd = "FINISHED";
-
-            }
+          
 
         }
         if(str.equals(NONE)){
